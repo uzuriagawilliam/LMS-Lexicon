6b08d1bdca12e67b3513fbf7c922adf3616d2595
sunisakc <sunisa.kan@gmail.com>
2021-10-08 11:12:23 +0200

    Added Model classes

---
+++ LMS-Lexicon/Models/Entities/ActivityType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS_Lexicon.Models.Entities
+{
+    public class ActivityType
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(25), MinLength(2)]
+        
+        public string Name { get; set; }
+        public int DocumentId { get; set; }
+        public int ActivityId { get; set; }
+
+    }
+}
---
+++ LMS-Lexicon/Models/Entities/Module.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS_Lexicon.Models.Entities
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(25), MinLength(2)]
+        public string Name { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime StartDate { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime EndDate { get; set; }
+        public int CourseId { get; set; }
+        
+        [MaxLength(300)]
+        public string Description { get; set; }
+        public int DocumentId { get; set; }
+        public int ActivityId { get; set; }
+    }
+}
