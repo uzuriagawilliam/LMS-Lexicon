181ccdfd904c878d5fbfd2b5018070faa5d458f3
yoque <65117031+yoque@users.noreply.github.com>
2021-10-14 10:50:23 +0200

    skapa kurs

---
+++ LMS-Lexicon/Controllers/LMS.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS_Lexicon.Controllers
+{
+    public class LMS : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
